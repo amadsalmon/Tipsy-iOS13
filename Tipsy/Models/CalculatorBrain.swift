e82c3abe75d454b8622ad9601687873e844abd9b
Amad Salmon <amad.salmon@icloud.com>
2020-07-20 16:22:26 +0200

    Starting to adopt MVC pattern

---
+++ Tipsy/Models/CalculatorBrain.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorBrain.swift
+//  Tipsy
+//
+//  Created by Amad Salmon on 20/07/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    var billTotal:Float?
+    var selectedTip:Float?
+    var splitNumber:Int?
+    var calculatedPerPerson:Float?
+
+    func calculateBillTotalWithTip() {
+        
+    }
+}
+
+
